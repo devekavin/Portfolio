2f6e0e7526770e96235ff3b75556dfe42c0cff17
kavin <kavinsvkn@gmail.com>
2025-04-08 12:34:30 +0530

    hosting the website

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 console.log("%c Kavin!!", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113);padding:20px;");
-console.log("%cHey Curious, If you like my work feel free to contact me @Kavin", "color:greenyellow; font-family:monospace; font-size: 15px");
+console.log("%cHey Curious, If you like my work feel free to contact me @Kavin", "color:#adff2f; font-family:monospace; font-size: 15px");
 // copyright year
 let $4fa36e821943b400$var$currentYear = new Date();
 document.getElementById("year").innerHTML = $4fa36e821943b400$var$currentYear.getFullYear();
-// toggle button
-document.getElementById("switch").addEventListener("click", function() {
-    let bodyTheme = document.body;
-    bodyTheme.classList.toggle("theme");
+// theme
+const $4fa36e821943b400$var$bodyTheme = document.body;
+const $4fa36e821943b400$var$switchButton = document.getElementById("switch");
+const $4fa36e821943b400$var$prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+let $4fa36e821943b400$var$storedTheme = localStorage.getItem("theme");
+if ($4fa36e821943b400$var$storedTheme) {
+    if ($4fa36e821943b400$var$storedTheme === "light") $4fa36e821943b400$var$bodyTheme.classList.add("light-theme");
+    else $4fa36e821943b400$var$bodyTheme.classList.add("dark-theme");
+} else if ($4fa36e821943b400$var$prefersDarkScheme.matches) $4fa36e821943b400$var$bodyTheme.classList.add("dark-theme");
+else $4fa36e821943b400$var$bodyTheme.classList.add("light-theme");
+// Theme toggle
+$4fa36e821943b400$var$switchButton.addEventListener("click", ()=>{
+    if ($4fa36e821943b400$var$bodyTheme.classList.contains("dark-theme")) {
+        $4fa36e821943b400$var$bodyTheme.classList.replace("dark-theme", "light-theme");
+        localStorage.setItem("theme", "light");
+    } else {
+        $4fa36e821943b400$var$bodyTheme.classList.replace("light-theme", "dark-theme");
+        localStorage.setItem("theme", "dark");
+    }
 });
 // scroll to top
 document.getElementById("scrollBtn1").addEventListener("click", function() {
